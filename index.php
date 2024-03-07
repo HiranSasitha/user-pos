06272b232d6b02e742543e5f55f3149286984356
HiranSasitha <hiransasitha123@gamil.com>
2024-03-07 15:25:04 +0530

    test

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: *");
-    echo "test";
+    include 'dbc.php';
+
+   $conn = new DbConnect();
+    $db = $conn->connect();
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    switch($method) {
+        case 'POST':
+            $user = json_decode(file_get_contents('php://input'));
+            $sql = "INSERT INTO users( name, email) values(:name, :email)";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':name', $user->name);
+            $stmt->bindParam(':email', $user->email);
+            if($stmt->execute()) {
+                $data = ['status' => 1, 'message' => "Record successfully created"];
+            } else {
+                $data = ['status' => 0, 'message' => "Failed to create record."];
+            }
+            echo json_encode($data);
+            break;
+}
     
 ?>
