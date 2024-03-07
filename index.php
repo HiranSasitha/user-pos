876fbf54471ee5f80df83d7f87f53d285f964d29
HiranSasitha <hiransasitha123@gamil.com>
2024-03-07 17:36:47 +0530

    completed add user

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: *");
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Allow-Methods: *');
     include 'dbc.php';
 
    $conn = new DbConnect();
